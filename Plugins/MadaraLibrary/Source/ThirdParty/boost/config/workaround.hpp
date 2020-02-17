19b382a0aa652167342cad3504973080b52584b5
James Edmondson <jedmondson@gmail.com>
2020-02-17 15:21:08 -0800

    * Fixed clang compilation warning/error in UE4 Boost builds. This was caused by the WORKAROUND_GUARD being used for checking clang versions without defining the appropriate #defines first. Not our fault. Definitely a boost bug

--- Plugins/MadaraLibrary/Source/ThirdParty/boost/config/workaround.hpp
+++ Plugins/MadaraLibrary/Source/ThirdParty/boost/config/workaround.hpp
@@ -70,6 +70,11 @@
 #else
 #define BOOST_MSVC_FULL_VER_WORKAROUND_GUARD 0
 #endif
+#ifndef __clang_major__
+#define __clang_major___WORKAROUND_GUARD 1
+#else
+#define __clang_major___WORKAROUND_GUARD 0
+#endif
 #ifndef __GNUC__
 #define __GNUC___WORKAROUND_GUARD 1
 #else
