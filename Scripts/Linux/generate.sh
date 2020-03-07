c2c09267f45da7e3ab28f99bab635dbb1d4b34c4
James Edmondson <jedmondson@gmail.com>
2020-03-07 11:42:39 -0800

    * Added VS code and Linux-generated makefiles to .gitignore * Updated Linux generate script to create Binaries/Linux

--- Scripts/Linux/generate.sh
+++ Scripts/Linux/generate.sh
@@ -5,4 +5,5 @@ echo "----------------------"
 echo "UE4_ROOT=$UE4_ROOT"
 echo "SCRIPTS_DIR=$SCRIPTS_DIR"
 echo "----------------------"
+mkdir -p "$SCRIPTS_DIR/../../Binaries/Linux"
 "$UE4_ROOT"/GenerateProjectFiles.sh -project="$SCRIPTS_DIR/../../GamsPlugins.uproject" -game -engine -Makefile -vscode
