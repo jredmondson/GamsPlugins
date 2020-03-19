e009986648656c46b9b356d7f0afef8e21f26c99
James Edmondson <jedmondson@gmail.com>
2020-03-19 03:36:25 -0700

    * Added version file and set it to 0.0.5 * Updated GamsAgents plugin version to 0.0.5 * Added package.bat for Windows which cooks and zips up a release version

---
+++ scripts/windows/package.bat
@@ -0,0 +1,27 @@
+@echo off
+
+SET SCRIPTS_DIR=%~dp0
+SET SCRIPTS_DIR=%SCRIPTS_DIR:~0,-1%
+SET PROJECT_ROOT=%SCRIPTS_DIR:~0,-16%
+SET PROJECT_FILE=%PROJECT_ROOT%\GamsPlugins.uproject
+echo.
+echo Running GamsPlugins in EDITOR MODE
+echo -------Settings-------
+echo UE4_ROOT=%UE4_ROOT%
+echo SCRIPTS_DIR=%SCRIPTS_DIR%
+echo PROJECT_ROOT=%PROJECT_ROOT%
+echo PROJECT_FILE=%PROJECT_FILE%
+echo ----------------------
+
+set /p VERSION=<"%PROJECT_ROOT%version.txt"
+
+setlocal enableextensions
+md "%SCRIPTS_DIR%\..\..\Binaries\Win64"
+
+call "%UE4_ROOT%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%PROJECT_FILE%" -noP4 -build -cook -stage -stagingdirectory="%PROJECT_ROOT%\out" -pak
+
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command ^ Compress-Archive -Force -Path "%PROJECT_ROOT%\out\WindowsNoEditor\*" -DestinationPath "%PROJECT_ROOT%\GamsPlugins_Win64_v%VERSION%.zip"
+
+echo Finished with script
+
+endlocal
